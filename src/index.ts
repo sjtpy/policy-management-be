36b7f0414649688fafe9702e5b3681a9efc57d0c
sjtpy <sujitclick7.sp@gmail.com>
2025-08-07 04:13:11 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 
 // CORS middleware
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3000'],
     credentials: true
 }));
 
